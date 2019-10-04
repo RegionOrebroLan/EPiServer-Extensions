dcaa662ce6c64bd432c28bab5b33a86ef206f563
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2019-10-04 17:14:55 +0200

    New function-filter.

---
+++ Source/Project/Filters/FunctionFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using EPiServer.Core;
+using EPiServer.Filters;
+
+namespace RegionOrebroLan.EPiServer.Filters
+{
+	public class FunctionFilter : ContentFilter
+	{
+		#region Constructors
+
+		public FunctionFilter(Func<IContent, bool> filterFunction)
+		{
+			this.FilterFunction = filterFunction ?? throw new ArgumentNullException(nameof(filterFunction));
+		}
+
+		#endregion
+
+		#region Properties
+
+		protected internal virtual Func<IContent, bool> FilterFunction { get; }
+
+		#endregion
+
+		#region Methods
+
+		public static IContentFilter Create(Func<IContent, bool> filterFunction)
+		{
+			if(filterFunction == null)
+				throw new ArgumentNullException(nameof(filterFunction));
+
+			return new FunctionFilter(filterFunction);
+		}
+
+		public override bool ShouldFilter(IContent content)
+		{
+			return this.FilterFunction(content);
+		}
+
+		#endregion
+	}
+}
